45b048cc3ada6b177ac481a9448f1d592d0a020f
ChotaiP <punit.chotai7@gmail.com>
2024-07-28 19:58:17 +0530

    Pure Pledge Publica

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+
+
+			<b class="copyright"> © Copyright 2024 .  Pure Pledge Publica </b>
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+                © Copyright 2024 .  Pure Pledge Publica
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
